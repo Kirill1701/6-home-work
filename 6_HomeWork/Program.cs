17323bd282a4360aa03769e17a6e4931c4a905ae
Kirill Vasilenko <kirvasilenko17@icloud.com>
2022-09-19 22:51:33 +1000

    final version

--- 6_HomeWork/Program.cs
+++ 6_HomeWork/Program.cs
@@ -11,53 +11,62 @@ namespace _6_HomeWork
     {
         static void Main(string[] args)
         {
-            using (StreamWriter sw = new StreamWriter("staff.csv", true, Encoding.Unicode))
+            Console.Write("Вывести данные на экран - введите 1; \nЗаполнить данные - введите 2: ");
+            int action = int.Parse(Console.ReadLine());
+
+            if (action == 2)
             {
-                char key = 'д';
-
-                do
+                using (StreamWriter sw = new StreamWriter("staff.csv", true, Encoding.Unicode))
                 {
-                    string guide = string.Empty;
-                    Console.Write("\nВведите ID сотрудника: ");
-                    guide += $"{Console.ReadLine()}\t";
+                    char key = 'д';
 
-                    string now = DateTime.Now.ToShortDateString();
-                    string time = DateTime.Now.ToShortTimeString();
-                    Console.WriteLine($"Дата и время добавления записи {now} {time}");
-                    guide += $"{now} {time}\t";
+                    do
+                    {
+                        string guide = string.Empty;
+                        Console.Write("\nВведите ID сотрудника: ");
+                        guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите Ф.И.О. сотрудника: ");
-                    guide += $"{Console.ReadLine()}\t";
+                        string now = DateTime.Now.ToShortDateString();
+                        string time = DateTime.Now.ToShortTimeString();
+                        Console.WriteLine($"Дата и время добавления записи {now} {time}");
+                        guide += $"{now} {time}\t";
 
-                    Console.Write("Введите возраст сотрудника: ");
-                    guide += $"{Console.ReadLine()}\t";
+                        Console.Write("Введите Ф.И.О. сотрудника: ");
+                        guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите рост сотрудника: ");
-                    guide += $"{Console.ReadLine()}\t";
+                        Console.Write("Введите возраст сотрудника: ");
+                        guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите дату рождения сотрудника: ");
-                    guide += $"{Console.ReadLine()}\t";
+                        Console.Write("Введите рост сотрудника: ");
+                        guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите место рождения сотрудника: ");
-                    guide += $"{Console.ReadLine()}\t";
-                    sw.WriteLine(guide);
-                    Console.Write("Продолжить н/д"); key = Console.ReadKey(true).KeyChar;
-                }
-                while (char.ToLower(key) == 'д');
-            }
+                        Console.Write("Введите дату рождения сотрудника: ");
+                        guide += $"{Console.ReadLine()}\t";
 
-            using (StreamReader sr = new StreamReader("staff.csv", Encoding.Unicode))
-            {
-                string line;
-                Console.WriteLine();
-
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] staff = line.Split('\t');
-                    Console.WriteLine($"{staff[0], 8} {staff[1], 15} {staff[2], 30} {staff[3], 5} {staff[4], 5} {staff[5], 15} {staff[6], 25}");
+                        Console.Write("Введите место рождения сотрудника: ");
+                        guide += $"{Console.ReadLine()}\t";
+                        sw.WriteLine(guide);
+                        Console.Write("Продолжить н/д"); key = Console.ReadKey(true).KeyChar;
+                    }
+                    while (char.ToLower(key) == 'д');
                 }
             }
+            else if (action == 1)
+            {
+                using (StreamReader sr = new StreamReader("staff.csv", Encoding.Unicode))
+                {
+                    string line;
+                    Console.WriteLine();
 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] staff = line.Split('\t');
+                        Console.WriteLine($"{staff[0],8} {staff[1],15} {staff[2],30} {staff[3],5} {staff[4],5} {staff[5],15} {staff[6],25}");
+                    }
+                }
+            }
+            else Console.WriteLine("Некорректные данные. Перезапустите программу.");
+            
         }
     }
 }
