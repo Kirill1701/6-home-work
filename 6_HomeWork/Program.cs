67bbb571dbd2c829751e6f6bb014542b0307b86e
Kirill Vasilenko <kirvasilenko17@icloud.com>
2022-09-19 17:57:24 +1000

    2-read-file

--- 6_HomeWork/Program.cs
+++ 6_HomeWork/Program.cs
@@ -18,27 +18,27 @@ namespace _6_HomeWork
                 do
                 {
                     string guide = string.Empty;
-                    Console.Write("\nВведите ID сотрудника: ", 10);
+                    Console.Write("\nВведите ID сотрудника: ");
                     guide += $"{Console.ReadLine()}\t";
 
                     string now = DateTime.Now.ToShortDateString();
                     string time = DateTime.Now.ToShortTimeString();
-                    Console.WriteLine($"Дата и время добавления записи {now} {time}", 20);
+                    Console.WriteLine($"Дата и время добавления записи {now} {time}");
                     guide += $"{now} {time}\t";
 
-                    Console.Write("Введите Ф.И.О. сотрудника: ", 25);
+                    Console.Write("Введите Ф.И.О. сотрудника: ");
                     guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите возраст сотрудника: ", 5);
+                    Console.Write("Введите возраст сотрудника: ");
                     guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите рост сотрудника: ", 5);
+                    Console.Write("Введите рост сотрудника: ");
                     guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите дату рождения сотрудника: ", 15);
+                    Console.Write("Введите дату рождения сотрудника: ");
                     guide += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Введите место рождения сотрудника: ", 25);
+                    Console.Write("Введите место рождения сотрудника: ");
                     guide += $"{Console.ReadLine()}\t";
                     sw.WriteLine(guide);
                     Console.Write("Продолжить н/д"); key = Console.ReadKey(true).KeyChar;
@@ -50,6 +50,12 @@ namespace _6_HomeWork
             {
                 string line;
                 Console.WriteLine();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] staff = line.Split('\t');
+                    Console.WriteLine($"{staff[0], 8} {staff[1], 15} {staff[2], 30} {staff[3], 5} {staff[4], 5} {staff[5], 15} {staff[6], 25}");
+                }
             }
 
         }
